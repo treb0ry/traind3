f74258fa394caca04d23e4f295c8db410f4d756b
treb0ry <57410485+treb0ry@users.noreply.github.com>
2019-11-09 11:51:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# js_initial_environment
+Начальная настройка для JavaScrip проектов. Настройка webpack, jest, babel.
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    hot: true,
+    contentBase: './dist',
+  },
+};

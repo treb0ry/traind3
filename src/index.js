964fce0868f27fb0662ffd6fd863217c0b168172
treb0ry <vinodel2589@gmai.com>
2019-11-27 21:44:40 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -777,9 +777,54 @@ async function createEvent() {
       .on("mouseout", null);
   }, 3000);
 }
+async function drawMap() {
+  const countryShapes = await d3.json("./world-geojson.json");
+  const dataset = await d3.csv("./data_population.csv");
+  const metric = "Population growth (annual %)";
+  const countryNameAccessor = d => d.properties["NAME_RU"];
+  const countryIdAccessor = d => d.properties["ADM0_A3_IS"];
+
+  let metricDataByCountry = {};
+  dataset.forEach(d => {
+    if (d["Series Name"] != metric) return;
+    metricDataByCountry[d["Country Code"]] = +d["2018 [YR2018]"] || 0;
+  });
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 10
+    }
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  const sphere = { type: "Sphere" };
+  const projection = d3
+    .geoEqualEarth()
+    .fitWidth(dimensions.boundedWidth, sphere);
+  const pathGenerator = d3.geoPath(projection);
+  const [[x0, y0], [x1, y1]] = pathGenerator.bounds(sphere);
+  dimensions.boundedHeight = y1;
+  dimensions.height =
+    dimensions.boundedHeight + dimensions.margin.top + dimensions.margin.bottom;
+  const wrapper = d3
+    .select("#map")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px,${dimensions.margin.top}px`
+    );
+}
+drawMap();
 //createEvent();
 //drawAnimBars();
 //drawHistogram();
 //drawScatterplot();
-drawLineChart();
+//drawLineChart();
 //metrics.forEach(drawHistgrams);
